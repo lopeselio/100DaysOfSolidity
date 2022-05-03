f08a19d9f651a6819a0ff8901adfab494b3456d6
lopeselio <lopeselio@gmail.com>
2022-05-03 23:29:50 +0530

    queue(): timestamp+queue pending

--- timelocks/TimeLock.sol
+++ timelocks/TimeLock.sol
@@ -2,7 +2,48 @@
 pragma solidity ^0.8.13;
 
 contract Timelock {
-    function queue() external {}
+    error NotOwnerError();
+    error AlreadyQueuedError(bytes32 txId);
+    address owner;
+    mapping(bytes32 => bool) public queued;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier OnlyOwner() {
+        if(msg.sender != owner) {
+            revert NotOwnerError();
+        }
+        _;
+    }
+
+    function getTxId(
+        address _target,
+        uint _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint _timestamp
+    ) public pure returns(bytes32 txId) {
+        return keccak256(
+            abi.encode(_target, _value, _func, _data, _timestamp)
+        );
+    }
+    function queue(
+        address _target,
+        uint _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint _timestamp
+    ) external OnlyOwner {
+        // create tx id
+        bytes32 txId = getTxId(_target, _value, _func, _data, _timestamp);
+        // check if tx unique
+        if (queued[txId]) {
+            revert AlreadyQueuedError(txId);
+        }
+        // check timestamp
+        // queue tx
+    }
     function execute() external {}
 
 }
