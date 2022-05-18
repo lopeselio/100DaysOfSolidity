0924ca71d468a1c3b96016a4fd0d2db2d4644331
lopeselio <lopeselio@gmail.com>
2022-05-18 12:03:40 +0530

    contract completed

--- NFTStakingGameFiContract/NFTeeStaker.sol
+++ NFTStakingGameFiContract/NFTeeStaker.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 import "hardhat/console.sol";
 
-contract NFTeeStaker is ERC20 {
+contract NFTeeStaker is ERC20, ReentrancyGuard {
     // What do we need ?
 
     // a way for people to stake 1 or more nfts
@@ -32,7 +34,7 @@ contract NFTeeStaker is ERC20 {
         // when was the last time rewards were calculated on-chain
         uint256 lastCheckpoint;
         // which NFTs were staked
-        uint256[] stakedNFTs; 
+        // uint256[] stakedNFTs; 
     }
     mapping(address => Staker) public stakers;
     mapping(uint256 => address) public tokenOwners;
@@ -60,24 +62,51 @@ contract NFTeeStaker is ERC20 {
             );
             tokenOwners[tokenIds[i]] = msg.sender;
             yield += BASE_YIELD_RATE;
-            user.stakedNFTs.push(tokenIds[i]);
+            // user.stakedNFTs.push(tokenIds[i]);
 
         }
         accumulate(msg.sender);
         user.currYield = yield;
     }
 
-    function unstake() public {
+    function unstake(uint256[] memory tokenIds) public {
+        Staker storage user = stakers[msg.sender];
+        uint256 yield = user.currYield;
+        uint256 length = tokenIds.length;
+        for(uint256 i=0; i<length; i++) {
+            require (
+                tokenOwners[tokenIds[i]] == msg.sender,
+                "NOT ORIGINAL OWNER"
+            );
+            require(nftContract.ownerOf(tokenIds[i])== address(this),
+            "NFT IS NOT STAKED"
+            );
+
+            tokenOwners[tokenIds[i]] = address(0);
+
+            if(yield != 0) {
+                yield -= BASE_YIELD_RATE;
+            }
+
+            nftContract.safeTransferFrom(address(this), msg.sender, tokenIds[i]);
+        }
+        accumulate(msg.sender);
+        user.currYield = yield;
 
     }
 
-    function claim() public {
+    function claim() public nonReentrant {
+        Staker storage user = stakers[msg.sender];
+        accumulate(msg.sender);
 
+        _mint(msg.sender, user.rewards);
+        user.rewards = 0;
     }
 
     function accumulate(address staker) internal {
-        stakers[staker].lastCheckpoint = block.timestamp;
         stakers[staker].rewards += getRewards(staker);
+        stakers[staker].lastCheckpoint = block.timestamp;
+
     }
 
     function getRewards(address staker) public view returns (uint256) {
