f48c0f2b0feebbfd04d9eb3ccdeb81904416e529
devELIOper <lopeselio@gmail.com>
2022-11-06 22:28:52 +0530

    withdraw() function

--- fcc-course/FundMe.sol
+++ fcc-course/FundMe.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT 
 
 pragma solidity ^0.8.8;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
+import "./PriceConverter.sol"
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 50 * 1e18;
 
@@ -11,29 +11,19 @@ contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough funds!"); // 1e18 = 1 * 10 ** 18 ETH
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough funds!"); // 1e18 = 1 * 10 ** 18 ETH
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function getPrice() public view returns(uint256) {
-        // ABI
-        // Address of oracle ETH/USD 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // ETH / USD
-        return uint256(price * 1e10);
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex = funderIndex + 1) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
 
-    }
-
-    function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd; 
     }
 }
