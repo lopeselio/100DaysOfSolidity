6fc9e759408cc7d4dce994c949bf8442cbcf7fc0
devELIOper <lopeselio@gmail.com>
2022-11-06 23:00:43 +0530

    modifier

--- fcc-course/FundMe.sol
+++ fcc-course/FundMe.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT 
 
-pragma solidity ^0.8.8;
-import "./PriceConverter.sol"
+pragma solidity ^0.8.0;
+import "./PriceConverter.sol";
+
 contract FundMe {
     using PriceConverter for uint256;
 
@@ -10,13 +11,21 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough funds!"); // 1e18 = 1 * 10 ** 18 ETH
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function withdraw() public {
+
+
+    function withdraw() public onlyOwner {
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex = funderIndex + 1) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
@@ -25,5 +34,10 @@ contract FundMe {
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
 
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not the owner");
+        _;
     }
 }
